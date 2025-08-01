c08aab19a1a022bd4cf62e8fa185c388af306ae7
reo0306 <reo19840306@gmail.com>
2025-08-01 09:16:50 +0900

    update app.vue, add components

--- sfc-basic/app/app.vue
+++ sfc-basic/app/app.vue
@@ -1,15 +1,15 @@
-<script setup lang="ts">
-const msg = ref("まだ");
-const onButtonClick = (label: string, event: Event): void => {
-  const target = event.target as HTMLButtonElement;
-  const text = target.innerHTML;
-  msg.value = `${label}と${text}`;
-};
-const url = "test";
-</script>
-
 <template>
-  <p>{{ msg }}</p>
-  <button v-on:click="onButtonClick('Hello', $event)">こんにちは</button>
-  <a v-bind:href="url" target="_blank">Nuxt.jsのサイト</a>
+  <section>
+    <h1>コンポーネント基礎</h1>
+    <OneSection/>
+    <OneSection/>
+    <PartsTheSupplement/>
+  </section>
 </template>
+
+<style>
+section {
+  border: blue 1px solid;
+  margin: 10px;
+}
+</style>
