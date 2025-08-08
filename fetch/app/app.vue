960a61153ced32866c0a09fbc37ca1f4ddb3cc20
reo0306 <reo19840306@gmail.com>
2025-08-08 17:36:42 +0900

    update refresh

--- fetch/app/app.vue
+++ fetch/app/app.vue
@@ -30,7 +30,7 @@ useState<Map<number, City>>(
 
 <template>
   <header>
-    <h1>useLazyAsyncDataサンプル</h1>
+    <h1>リフレッシュサンプル</h1>
   </header>
   <main>
     <NuxtPage/>
--- fetch/app/pages/index.vue
+++ fetch/app/pages/index.vue
@@ -1,20 +1,61 @@
 <script setup lang="ts">
 import type { City } from '~/interfaces';
 
+const config = useRuntimeConfig();
+const route = useRoute();
+
 const cityList = useState<Map<number, City>>("cityList");
+const selectedCityId = ref(1853909);
+const selectedCityInit = cityList.value.get(selectedCityId.value) as City;
+const selectedCity = ref(selectedCityInit);
+
+const params: {
+    lang: string,
+    q: string,
+    appid: string,
+} = 
+{
+    lang: "ja",
+    q: selectedCity.value.q,
+    appid: config.public.openWeatherApiKey,
+};
+
+const asyncData = await useLazyFetch(
+    "https://api.openweathermap.org/data/2.5/weather",
+    {
+        key: `/WeatherInfo/${route.params.id}`,
+        query: params,
+        transform: (data: any): string => {
+            const weatherArray = data.weather;
+            const weather = weatherArray[0];
+            return weather.description;
+        }
+    }
+);
+const pending = asyncData.pending;
+const weatherDescription = asyncData.data;
+const refresh = asyncData.refresh;
+
+const onCityChanged = () => {
+    selectedCity.value = cityList.value.get(selectedCityId.value) as City;
+    refresh();
+}
 </script>
 
 <template>
     <section>
-        <h2>都市リスト</h2>
-        <ul>
-            <li
-                v-for="[id, city] in cityList"
-                v-bind:key="id">
-                <NuxtLink v-bind:to="{name: 'WeatherInfo-id', params: {id: id}}">
-                    {{ city.name }}の天気
-                </NuxtLink>
-            </li>
-        </ul>
+        <label>
+            表示するお天気ポイント
+            <select v-model="selectedCityId" v-on:change="onCityChanged">
+                <option v-for="[id, city] in cityList" v-bind:key="id" v-bind:value="id">
+                    {{ city.name }}
+                </option>
+            </select>
+        </label>
+    </section>
+    <p v-if="pending">データ取得中⋯</p>
+    <section v-else>
+        <h2>{{ selectedCity.name }}の天気</h2>
+        <p>{{  weatherDescription }}</p>
     </section>
 </template>
