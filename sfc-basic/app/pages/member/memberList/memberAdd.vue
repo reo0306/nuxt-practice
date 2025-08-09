c2e77f049797a948b21a936a0aff07e4befff59f
reo0306 <reo19840306@gmail.com>
2025-08-09 22:53:33 +0900

    add MemberInfo

--- sfc-basic/app/pages/member/memberList/memberAdd.vue
+++ sfc-basic/app/pages/member/memberList/memberAdd.vue
@@ -8,7 +8,7 @@ useHead({
     title: PAGE_TITLE,
 });
 const router = useRouter();
-const memberList = useState<Map<number, Member>>("memberList") ;
+//const memberList = useState<Map<number, Member>>("memberList") ;
 const member: Member = reactive(
     {
         id: 0,
@@ -18,52 +18,65 @@ const member: Member = reactive(
         note: ""
     }
 );
-const onAdd = (): void => {
-    memberList.value.set(member.id, member);
-    router.push({name: "member-memberList"});
+const pending = ref(false);
+const onAdd = async (): Promise<void> => {
+    pending.value = true;
+    const asyncData = await useFetch(
+        "/api/addMemberInfo",
+        {
+            method: "POST",
+            body: member,
+        }
+    );
+    if (asyncData.data.value != null && asyncData.data.value.result == 1) {
+        router.push({name: "member-memberList"});
+    }
 }
 </script>
 
 <template>
     <section>
         <h2>{{ PAGE_TITLE }}</h2>
-        <p>
-            情報を入力し、登録ボタンをクリックしてください。
-        </p>
-        <form v-on:submit.prevent="onAdd">
-            <dl>
-                <dt>
-                    <label for="addId">ID&nbsp;</label>
-                </dt>
-                <dd>
-                    <input type="number" id="addId" v-model.number="member.id" required>
-                </dd>
-                <dt>
-                    <label for="addName">名前&nbsp;</label>
-                </dt>
-                <dd>
-                    <input type="text" id="addName" v-model="member.name" required>
-                </dd>
-                <dt>
-                    <label for="addEmail">メールアドレス&nbsp;</label>
-                </dt>
-                <dd>
-                    <input type="text" id="addEmail" v-model="member.email" required>
-                </dd>
-                <dt>
-                    <label for="addPoints">保有ポイント&nbsp;</label>
-                </dt>
-                <dd>
-                    <input type="number" id="addPoints" v-model.number="member.points" required>
-                </dd>
-                <dt>
-                    <label for="addNote">備考&nbsp;</label>
-                </dt>
-                <dd>
-                    <textarea id="addNote" v-model="member.note"></textarea>
-                </dd>
-            </dl>
-            <button type="submit">登録</button>
-        </form>
+        <p v-if="pending">データ取得中⋯</p>
+        <template v-else>
+            <p>
+                情報を入力し、登録ボタンをクリックしてください。
+            </p>
+            <form v-on:submit.prevent="onAdd">
+                <dl>
+                    <dt>
+                        <label for="addId">ID&nbsp;</label>
+                    </dt>
+                    <dd>
+                        <input type="number" id="addId" v-model.number="member.id" required>
+                    </dd>
+                    <dt>
+                        <label for="addName">名前&nbsp;</label>
+                    </dt>
+                    <dd>
+                        <input type="text" id="addName" v-model="member.name" required>
+                    </dd>
+                    <dt>
+                        <label for="addEmail">メールアドレス&nbsp;</label>
+                    </dt>
+                    <dd>
+                        <input type="text" id="addEmail" v-model="member.email" required>
+                    </dd>
+                    <dt>
+                        <label for="addPoints">保有ポイント&nbsp;</label>
+                    </dt>
+                    <dd>
+                        <input type="number" id="addPoints" v-model.number="member.points" required>
+                    </dd>
+                    <dt>
+                        <label for="addNote">備考&nbsp;</label>
+                    </dt>
+                    <dd>
+                        <textarea id="addNote" v-model="member.note"></textarea>
+                    </dd>
+                </dl>
+                <button type="submit">登録</button>
+            </form>
+        </template>
     </section>
 </template>
---
+++ sfc-basic/server/api/addMemberInfo.ts
@@ -0,0 +1,10 @@
+export default defineEventHandler(
+    async (event) => {
+        const body = await readBody(event);
+        console.log(body);
+        return {
+            result: 1,
+            member: body,
+        };
+    }
+)
