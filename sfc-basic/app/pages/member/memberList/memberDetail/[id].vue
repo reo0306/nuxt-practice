0fce3c215ec7ae8e2bf484e0aaab607caae9feaf
reo0306 <reo19840306@gmail.com>
2025-08-09 22:42:23 +0900

    add getOneMemberInfo

--- sfc-basic/app/pages/member/memberList/memberDetail/[id].vue
+++ sfc-basic/app/pages/member/memberList/memberDetail/[id].vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import type {Member} from "~/interfaces";
+//import type {Member} from "~/interfaces";
+
 const PAGE_TITLE = "会員詳細情報";
 definePageMeta({
     layout: "member"
@@ -8,17 +9,18 @@ useHead({
     title: PAGE_TITLE,
 });
 const route = useRoute();
-const memberList = useState<Map<number, Member>>("memberList");
-const member = computed(
-    (): Member => {
-        const id = Number(route.params.id);
-        return memberList.value.get(id) as Member;
+const asyncData = useLazyFetch(
+    "/api/getOneMemberInfo",
+    {
+        query: {id: route.params.id}
     }
 );
+const member = asyncData.data;
+const pending = asyncData.pending;
 const localNote = computed(
     (): string => {
         let localNote = "--";
-        if (member.value.note != undefined) {
+        if (member.value != null && member.value.note != undefined) {
             localNote = member.value.note;
         }
         return localNote;
@@ -29,17 +31,18 @@ const localNote = computed(
 <template>
     <section>
         <h2>{{ PAGE_TITLE }}</h2>
-        <dl>
+        <p v-if="pending">データ取得中⋯</p>
+        <dl v-else>
             <dt>ID</dt>
-            <dd>{{ member.id }}</dd>
+            <dd>{{ member?.id }}</dd>
             <dt>名前</dt>
-            <dd>{{ member.name }}</dd>
+            <dd>{{ member?.name }}</dd>
             <dt>メールアドレス</dt>
-            <dd>{{ member.email }}</dd>
+            <dd>{{ member?.email }}</dd>
             <dt>保有ポイント</dt>
-            <dd>{{ member.points }}</dd>
+            <dd>{{ member?.points }}</dd>
             <dt>備考</dt>
-            <dd>{{ member.note }}</dd>
+            <dd>{{ localNote }}</dd>
         </dl>
     </section>
 </template>
