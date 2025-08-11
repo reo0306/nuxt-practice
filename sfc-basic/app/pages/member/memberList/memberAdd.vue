1e76ef62fa8adefb55bfa9332e5a9bb01b12fd73
reo0306 <reo19840306@gmail.com>
2025-08-11 14:18:59 +0900

    add post server error

--- sfc-basic/app/pages/member/memberList/memberAdd.vue
+++ sfc-basic/app/pages/member/memberList/memberAdd.vue
@@ -18,6 +18,7 @@ const member: Member = reactive(
     }
 );
 const pending = ref(false);
+const noServerError = ref(true);
 const onAdd = async (): Promise<void> => {
     pending.value = true;
     const asyncData = await useFetch(
@@ -27,8 +28,11 @@ const onAdd = async (): Promise<void> => {
             body: member,
         }
     );
-    if (asyncData.data.value != null && asyncData.data.value.result == 1) {
+    if (asyncData.error.value == null && asyncData.data.value != null && asyncData.data.value.result == 1) {
         router.push({name: "member-memberList"});
+    } else {
+        pending.value = false;
+        noServerError.value = false;
     }
 }
 </script>
@@ -38,8 +42,11 @@ const onAdd = async (): Promise<void> => {
         <h2>{{ PAGE_TITLE }}</h2>
         <p v-if="pending">データ取得中⋯</p>
         <template v-else>
-            <p>
+            <p v-if="noServerError">
                 情報を入力し、登録ボタンをクリックしてください。
+            </p>
+            <p v-else>
+                サーバ処理中に障害が発生しました。もう一度登録を行ってください。
             </p>
             <form v-on:submit.prevent="onAdd">
                 <dl>
--- sfc-basic/server/routes/member-management/members.post.ts
+++ sfc-basic/server/routes/member-management/members.post.ts
@@ -1,20 +1,31 @@
 import type { Member, ReturnJSONMembers } from "@/interfaces";
+import { renderSlot } from "vue";
 
 export default defineEventHandler(
     async (event): Promise<ReturnJSONMembers> => {
-        const body = await readBody(event);
-        const member = body as Member;
-        let memberList = new Map<number, Member>();
-        const storage = useStorage();
-        const memberListStorage = await storage.getItem("local:member-management_member");
-        if (memberListStorage != undefined) {
-            memberList = new Map<number, Member>(memberListStorage as any);
+        let resultVal = 0;
+        const memberListArray : Member[] = [];
+
+        // throw createError("疑似エラー発生");
+        try {
+            const body = await readBody(event);
+            const member = body as Member;
+            let memberList = new Map<number, Member>();
+            const storage = useStorage();
+            const memberListStorage = await storage.getItem("local:member-management_member");
+            if (memberListStorage != undefined) {
+                memberList = new Map<number, Member>(memberListStorage as any);
+            }
+            memberList.set(member.id, member);
+            await storage.setItem("local:member-management_members", [...memberList]);
+            memberListArray[0] = member;
+            resultVal = 1;
+        } catch(err) {
+            console.log(err);
         }
-        memberList.set(member.id, member);
-        await storage.setItem("local:member-management_members", [...memberList]);
         return {
-            result: 1,
-            data: [member]
+            result: resultVal,
+            data: memberListArray,
         }
     }
 );
